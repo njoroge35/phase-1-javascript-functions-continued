a837e5941b4820f07fe7235409d92c6e2a1ead04
David <david.njoroge@student.moringaschool.com>
2022-08-16 21:49:48 +0300

    js  functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+  }
+  
+  
+  function mondayWork(doing = 'go to the office') {
+    return (`This Monday, I will ${doing}.`);
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function (childParam = "loved") {
+      return `You are ${flair}${childParam}${flair}!`
+      } 
+  }
